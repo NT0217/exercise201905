e48e823490014980bfc057a44db8dde216aab2db
NT0217 <nakagawa.takuya@gmail.com>
2019-05-08 23:54:13 +0900

    add:ユーザー情報変更画面追加

--- ChatExercise/src/main/java/com/example/chat/controller/UserController.java
+++ ChatExercise/src/main/java/com/example/chat/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.chat.db.repository.UserRepository;
 import com.example.chat.db.entity.User;
@@ -54,5 +55,41 @@ public class UserController {
 		BeanUtils.copyProperties(form, user);
 		userService.save(user);
 		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public String edit(UserForm user) {
+		return "/edit";
+	}
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
+	public String edit(@Validated UserForm form, BindingResult result, @AuthenticationPrincipal LoginUserDetails loginUserDetails) {
+		User user = userService.findOne(loginUserDetails.getUserId());
+				
+		if(!form.getPassword().equals(form.getConfirmPassword())) {
+			result.rejectValue("password", "error.passwordConfirmation", "do not match.");
+		}
+		
+		if(userRepository.existsByName(form.getName())) {
+			result.rejectValue("name", "error.nameDuplication", "already exists.");
+		}
+		
+		if(result.hasErrors()) {
+			return "/edit";
+		}
+		
+		BeanUtils.copyProperties(form, user);
+		userService.save(user);
+		loginUserDetails.setUserId(user.getId());
+		loginUserDetails.setUserName(user.getName());
+		
+		return "redirect:/editSucces";
+	}
+	
+	@RequestMapping(value ="/editSucces", method = RequestMethod.GET)
+	public ModelAndView update(UserForm user, ModelAndView mav) {
+		mav.addObject("success", "変更に成功しました");
+		mav.setViewName("/edit");
+		return mav;
 	}
 }
--- ChatExercise/src/main/java/com/example/chat/security/LoginUserDetails.java
+++ ChatExercise/src/main/java/com/example/chat/security/LoginUserDetails.java
@@ -20,7 +20,16 @@ public class LoginUserDetails extends org.springframework.security.core.userdeta
     public Long getUserId() {
         return userId;
     }
+    
+    public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+    
     public String getUserName() {
         return userName;
     }
+    
+    public void setUserName(String userName) {
+		this.userName = userName;
+	}
 }
